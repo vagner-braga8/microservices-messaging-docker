de39bc717fd6945fbab6e29205e21206ae1af8cb
Vagner <haigorne@gmail.com>
2025-05-31 12:42:08 -0300

    [mod2] Configuração inicial do Discovery Server

---
+++ eurekaserver/src/test/java/io/github/vagner_braga8/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vagner_braga8.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
