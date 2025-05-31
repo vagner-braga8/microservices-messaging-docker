8db3d1c6d27e4303cbf422c9c37485b8d87a9a2a
Vagner <haigorne@gmail.com>
2025-05-31 13:01:19 -0300

    [mod2] Scaffolding do Microservice de Clientes

---
+++ msclientes/src/test/java/io/github/vagner_braga8/msclientes/MsclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vagner_braga8.msclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
