0db3b0c8c9ebcda44107760df4faa28de812f2a0
Vagner <haigorne@gmail.com>
2025-05-31 21:28:45 -0300

    [mod2] Criação do Cloud Gateway

---
+++ mscloudgateway/src/main/java/io/github/vagner_braga8/mscloudgateway/MscloudgatewayApplication.java
@@ -0,0 +1,29 @@
+package io.github.vagner_braga8.mscloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class MscloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/clientes/**") // Define um padrão de caminho para requisições HTTP
+						.uri("lb://msclientes")) // Redireciona as requisições para o serviço registrado com o nome "msclientes"
+				.build();
+	}
+
+}
