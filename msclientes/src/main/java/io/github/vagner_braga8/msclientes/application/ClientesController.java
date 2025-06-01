2f80236d7fe6c3b2e260e07c8ecfd52d034d6479
Vagner <haigorne@gmail.com>
2025-05-31 21:38:54 -0300

    [mod2] Checando o Load Balancer

--- msclientes/src/main/java/io/github/vagner_braga8/msclientes/application/ClientesController.java
+++ msclientes/src/main/java/io/github/vagner_braga8/msclientes/application/ClientesController.java
@@ -3,6 +3,7 @@ package io.github.vagner_braga8.msclientes.application;
 import io.github.vagner_braga8.msclientes.application.representation.ClienteSaveRequest;
 import io.github.vagner_braga8.msclientes.domain.Cliente;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,12 +14,14 @@ import java.util.Optional;
 @RestController
 @RequestMapping("clientes")
 @RequiredArgsConstructor
+@Slf4j
 public class ClientesController {
 
     private final ClienteService clienteService;
 
     @GetMapping
     public String status() {
+        log.info("msclientes status");
         return "OK";
     }
 
