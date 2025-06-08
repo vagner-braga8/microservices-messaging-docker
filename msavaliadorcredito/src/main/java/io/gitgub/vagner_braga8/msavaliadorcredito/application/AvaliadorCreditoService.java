08a172fd128566e831956cd23d265ddb57ea2006
Vagner <haigorne@gmail.com>
2025-06-08 13:29:00 -0300

    [mod3] Buscando os dados de cartoes pelo msavaliadorcredito

--- msavaliadorcredito/src/main/java/io/gitgub/vagner_braga8/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/io/gitgub/vagner_braga8/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,24 +1,31 @@
 package io.gitgub.vagner_braga8.msavaliadorcredito.application;
 
+import io.gitgub.vagner_braga8.msavaliadorcredito.domain.model.CartaoCliente;
 import io.gitgub.vagner_braga8.msavaliadorcredito.domain.model.DadosCliente;
 import io.gitgub.vagner_braga8.msavaliadorcredito.domain.model.SituacaoCliente;
+import io.gitgub.vagner_braga8.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import io.gitgub.vagner_braga8.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clienteResourceClient;
+    private final CartoesResourceClient cartoesResourceClient;
 
     public SituacaoCliente obterSituacaoCliente(String cpf) {
         ResponseEntity<DadosCliente> dadosClienteResponse = clienteResourceClient.dadosDoCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesResourceClient.getCartoesByCpf(cpf);
 
         return SituacaoCliente
                 .builder()
                 .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesResponse.getBody())
                 .build();
     }
 
---
+++ msavaliadorcredito/src/main/java/io/gitgub/vagner_braga8/msavaliadorcredito/infra/clients/CartoesResourceClient.java
@@ -0,0 +1,15 @@
+package io.gitgub.vagner_braga8.msavaliadorcredito.infra.clients;
+
+import io.gitgub.vagner_braga8.msavaliadorcredito.domain.model.CartaoCliente;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "mscartoes", path = "/cartoes")
+public interface CartoesResourceClient {
+    @GetMapping(params = "cpf")
+    ResponseEntity<List<CartaoCliente>> getCartoesByCpf(@RequestParam("cpf") String cpf);
+}
