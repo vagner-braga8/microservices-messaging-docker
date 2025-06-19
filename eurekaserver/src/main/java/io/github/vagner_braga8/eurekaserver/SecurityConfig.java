fc3d30dd41dd9910318c814ee8ad042b10f633a5
Vagner <haigorne@gmail.com>
2025-06-18 21:25:06 -0300

    [mod6] Protegendo o Discovery Server com Spring Security

---
+++ eurekaserver/src/main/java/io/github/vagner_braga8/eurekaserver/SecurityConfig.java
@@ -0,0 +1,20 @@
+package io.github.vagner_braga8.eurekaserver;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
